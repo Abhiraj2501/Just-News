44fff589ead1aa2a3e0bc275acd01c33fd122847
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-02-01 01:24:06 +0530

    hello

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print("hello")
-def fun(a):
-    random = a
-    print(random)
-fun(0)
+import requests 
+query = "Artificial Intelligence"
+api = "db72c882945b44daa4dde34b3c7044c8"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2025-12-31&sortBy=publishedAt&apiKey=db72c882945b44daa4dde34b3c7044c8"
+
+print(url)
+r = requests.get(url)
+data = r.json()
+articles = data["articles"]
+# for article in articles:
+print(articles[0]["title"],articles[0]["url"])
