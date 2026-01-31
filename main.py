b3110abb55b539b74e8f9ee81cf3bf0fc2dcb92d
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-02-01 01:32:45 +0530

    hello

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import requests 
-query = "Artificial Intelligence"
+import requests
+
+query = input("What news do you want? : ")
 api = "db72c882945b44daa4dde34b3c7044c8"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-12-31&sortBy=publishedAt&apiKey=db72c882945b44daa4dde34b3c7044c8"
+
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-12-31&sortBy=publishedAt&apiKey={api}"
 
 print(url)
+
 r = requests.get(url)
 data = r.json()
-articles = data["articles"]
-for index, article in enumerate(articles):
-    print(index+1,article["title"], article["url"])
+
+articles = data.get("articles", [])
+
+for index, article in enumerate(articles, start=1):
+    print(index, article.get("title"), article.get("url"))
     print("\n*****************************************************\n")
