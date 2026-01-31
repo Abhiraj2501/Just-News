6d2afb3abf8d486be4d9150ab881b74c983b223b
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-02-01 01:39:54 +0530

    hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from collections import Counter
 
 query = input("What news do you want? : ")
 api = "db72c882945b44daa4dde34b3c7044c8"
@@ -13,5 +14,15 @@ data = r.json()
 articles = data.get("articles", [])
 
 for index, article in enumerate(articles, start=1):
-    print(index, article.get("title"), article.get("url"))
+    print(index+1, article.get("title"), article.get("url"))
     print("\n*****************************************************\n")
+
+def keyword_frequency(keyword, articles):
+    count = 0
+    for article in articles:
+        title = article.get("title", "").lower()
+        count += title.count(keyword)
+    return count
+
+freq = keyword_frequency(query, articles)
+print(f"\nKeyword '{query}' appeared {freq} times in article titles")
